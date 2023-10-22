6b7f65e793dd5cc5f35afb3527a9951961995129
Niel <leincentes@gmail.com>
2023-10-22 12:25:10 +0800

    adjust css file

---
+++ EPCST/component/about.php
@@ -0,0 +1,11 @@
+<section id="about">
+    <div class="info-container">
+        <nav class="side-nav">
+
+        </nav>
+        <div class="text">
+
+        </div>
+    </div>
+    <img class="home-background" src="../img/home.jpg" alt="home">
+</section>
