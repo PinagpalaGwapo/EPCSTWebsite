04c4443ccd58daa8fdef33ade86126a0a87bb27e
Pinagpala <jeddcruzsalalila@gmail.com>
2023-10-23 17:04:39 +0800

    Added contact page

--- EPCST/component/contact.php
+++ EPCST/component/contact.php
@@ -1,3 +1,15 @@
-<section>
-    
-</section>
+<section id="contact">
+    <div class="contact-info">
+        <p class="contact-title">Contact Us</p>
+    </div>
+    <div class="contact-card">
+        <p class="address">Address: MGHP+QPQ, City of Balanga, Bataan</p>
+        <p class="contact-number">(047) 791 2791</p>
+        <p class="school-email">eastwoodsprofessional@yahoo.com</p>
+        <p class="duration-operation">Operation Hours:</p>
+        <p class="monday-fri">Monday to Friday 8:00 am to 8:00 pm</p>
+        <p class="saturday">Saturday: 8:00 am to 3:00 pm</p>
+        <p class="sunday">Sunday: CLOSED</p>
+    </div>
+</section> 
+>>>>>>> 2a28c63 (Added contact page)
