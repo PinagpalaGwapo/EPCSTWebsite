98f819fa521f9b7bfe6bd4a877ba0730b65058c9
Niel <leincentes@gmail.com>
2023-10-24 20:46:24 +0800

    adjust contact page

--- EPCST/component/contact.php
+++ EPCST/component/contact.php
@@ -4,13 +4,26 @@
         <h1 class="contact-title">Contact Us</h1>
     </div>
     <div class="contact-card">
+        <div>
         <p class="address">Address: MGHP+QPQ, City of Balanga, Bataan</p>
         <p class="contact-number">(047) 791 2791</p>
         <p class="school-email">eastwoodsprofessional@yahoo.com</p>
         <p class="duration-operation">Operation Hours:</p>
-        <p class="monday-fri">Monday to Friday 8:00 am to 8:00 pm</p>
-        <p class="saturday">Saturday: 8:00 am to 3:00 pm</p>
-        <p class="sunday">Sunday: CLOSED</p>
+            <div class="do-container">
+            <p class="monday-fri">Monday to Friday 8:00 am to 8:00 pm</p>
+            <p class="saturday">Saturday: 8:00 am to 3:00 pm</p>
+            <p class="sunday">Sunday: CLOSED</p>
+            </div>
+        </div>
+        
+        <div>
+            <!-- Map -->
+            Map
+        </div>
+       
         <!-- <p class="iframe-container"<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d906.6123639058899!2d120.53696171540155!3d14.679641942316366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x339641c0395eb9e9%3A0x7d3d02d68a830230!2sEASTWOODS%20Professional%20College%20of%20Science%20and%20Technology!5e0!3m2!1sen!2sph!4v1698121573970!5m2!1sen!2sph" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>-->
+    </div>
+    <div class="messenger">
+        <a href="https://www.facebook.com/EPCST/?_rdc=1&_rdr" target="_blank"><img src="../img/messenger.png" alt="messnger_contact" class="messenger-img"></a>
     </div>
 </section> 
