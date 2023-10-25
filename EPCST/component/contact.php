b432f47bfeac24a8d88a302b6c6c52f67035dfdd
Pinagpala <jeddcruzsalalila@gmail.com>
2023-10-25 10:44:38 +0800

    Map adjustment in contact page

--- EPCST/component/contact.php
+++ EPCST/component/contact.php
@@ -18,12 +18,12 @@
         
         <div class="map">
             <!-- Map -->
-            <p class="iframe-container"<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d906.6123639058899!2d120.53696171540155!3d14.679641942316366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x339641c0395eb9e9%3A0x7d3d02d68a830230!2sEASTWOODS%20Professional%20College%20of%20Science%20and%20Technology!5e0!3m2!1sen!2sph!4v1698121573970!5m2!1sen!2sph" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1147.4598205596685!2d120.53647958338185!3d14.67980376558722!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x339641c0395eb9e9%3A0x7d3d02d68a830230!2sEASTWOODS%20Professional%20College%20of%20Science%20and%20Technology!5e0!3m2!1sen!2sph!4v1698200642533!5m2!1sen!2sph" width="400" height="300" style="border: 2px;;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
        
         
     </div>
     <div class="messenger">
-        <a href="https://www.facebook.com/EPCST/?_rdc=1&_rdr" target="_blank"><img src="../img/messenger.png" alt="messnger_contact" class="messenger-img"></a>
+        <a href="https://www.facebook.com/EPCST/?_rdc=1&_rdr" target="_blank"><img src="../img/messenger.png" alt="messenger_contact" class="messenger-img"></a>
     </div>
 </section> 
