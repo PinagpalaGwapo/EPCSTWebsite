856f7355429eae6e751e6975879fd0735ce172a7
Kyle2529 <148669574+Kyle2529@users.noreply.github.com>
2023-10-25 23:45:41 +0800

    Update about.php
    about page Kyle babaguhin ko pa mamayang umaga

--- EPCST/component/about.php
+++ EPCST/component/about.php
@@ -1,11 +1,34 @@
 <section id="about">
+
+
+<html lang="en">
+<head>
+
+    <div class="white-column">
+ <ul class="navigation">
+            <li><a href="#">History</a></li>
+            <li><a href="#">Mission</a></li>
+            <li><a href="#">Vision</a></li>
+            <li><a href="#">Philisophy and Values</a></li>
+            <li><a href="#">Facilities</a></li>
+            <li><a href="#">Departments</a></li>
+        </ul>
+</div>
+ 
+</html>
+
+
     <div class="info-container">
         <nav class="side-nav">
+
+
 
         </nav>
         <div class="text">
 
+
         </div>
     </div>
-    <img class="home-background" src="./img/home.jpg" alt="home">
+   
 </section>
+
